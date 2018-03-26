bb950b13ace49e0ab64450facfbea6f4ad8d45d1
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-26 12:41:10 -0500

    login form style

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -27,9 +27,9 @@
                                                         {{ csrf_field() }}
                                 
                                                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                                                            <label for="email" class="col-md-12 control-label">E-Mail Address</label>
                                 
-                                                            <div class="col-md-6">
+                                                            <div class="col-md-12">
                                                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                                 
                                                                 @if ($errors->has('email'))
@@ -41,9 +41,9 @@
                                                         </div>
                                 
                                                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                                            <label for="password" class="col-md-4 control-label">Password</label>
+                                                            <label for="password" class="col-md-12 control-label">Password</label>
                                 
-                                                            <div class="col-md-6">
+                                                            <div class="col-md-12">
                                                                 <input id="password" type="password" class="form-control" name="password" required>
                                 
                                                                 @if ($errors->has('password'))
@@ -55,7 +55,7 @@
                                                         </div>
                                 
                                                         <div class="form-group">
-                                                            <div class="col-md-6 col-md-offset-4">
+                                                            <div class="col-md-12">
                                                                 <div class="checkbox">
                                                                     <label>
                                                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
@@ -65,7 +65,7 @@
                                                         </div>
                                 
                                                         <div class="form-group">
-                                                            <div class="col-md-8 col-md-offset-4">
+                                                            <div class="col-md-12">
                                                                 <button type="submit" class="btn btn-primary">
                                                                     Login
                                                                 </button>
@@ -79,7 +79,7 @@
                                                         <p style="margin-left:265px">OR</p>
                                                         <br />
                                                         <div class="form-group">
-                                                            <div class="col-md-8 col-md-offset-4">
+                                                            <div class="col-md-12">
                                                               <a href="{{url('/redirect')}}" class="btn btn-primary">Login with Facebook</a>
                                                             </div>
                                                         </div>
